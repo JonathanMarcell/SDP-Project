ce35f14404d50c25048eba532ca49186926e1a63
joshuanando <71019558+joshuanando@users.noreply.github.com>
2021-03-25 14:24:11 +0700

    update kasir

--- ELREORS/ELREORS/Kasir.xaml.cs
+++ ELREORS/ELREORS/Kasir.xaml.cs
@@ -23,5 +23,55 @@ namespace ELREORS
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 2";
+        }
+
+        private void btn3_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 3";
+        }
+
+        private void btn4_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 4";
+        }
+
+        private void btn5_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 5";
+        }
+
+        private void btn6_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 6";
+        }
+
+        private void btn7_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 7";
+        }
+
+        private void btn8_Click_1(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA 8";
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            mejudul.Content = "MEJA";
+        }
     }
 }
