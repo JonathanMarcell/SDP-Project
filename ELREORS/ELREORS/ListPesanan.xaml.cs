a2cebb4f41e165ea27a1afe3a88c0eab1a0ab8ef
Davin <christopherdavin99@gmail.com>
2021-05-06 22:59:08 +0700

    List Pesanan Up
    update djual status jadi 1

--- ELREORS/ELREORS/ListPesanan.xaml.cs
+++ ELREORS/ELREORS/ListPesanan.xaml.cs
@@ -39,16 +39,30 @@ namespace ELREORS
             dtPesanan.Columns.Add("Action");
             loadData();
 
-            
         }
 
         private void dgPesanan_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            hapusData();
+            try
+            {
+                int index = Int32.Parse(tempData.Rows[dgPesanan.SelectedIndex][0].ToString());
+                
+                string qry = $"update djual set status=1 where id={index}";
+                conn.Open();
+                OracleCommand cmd = new OracleCommand(qry, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                hapusData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
 
         public void hapusData()
         {
+            tempData.Rows[dgPesanan.SelectedIndex].Delete();
             dtPesanan.Rows[dgPesanan.SelectedIndex].Delete();
            
         }
@@ -58,13 +72,14 @@ namespace ELREORS
             {
                 conn.Open();
                 tempData = new DataTable();
-                daPesanan = new OracleDataAdapter("select m.nama, h.nomor_meja, d.jumlah, d.keterangan from menu m, djual d, hjual h where d.id_header=h.id and d.id_menu=m.id and h.status=0", conn);
+                daPesanan = new OracleDataAdapter("select d.id, m.nama, h.nomor_meja, d.jumlah, d.keterangan from menu m, djual d, hjual h where d.id_header=h.id and d.id_menu=m.id and h.status=0 and d.status=0", conn);
                 daPesanan.Fill(tempData);
                 foreach (DataRow d in tempData.Rows) 
                 {
                     dtPesanan.Rows.Add(d["nama"],"Meja "+d["nomor_meja"],d["jumlah"],d["keterangan"]);
                 }
                 dgPesanan.ItemsSource = dtPesanan.DefaultView;
+                conn.Close();
             }
             catch (Exception ex)
             {
