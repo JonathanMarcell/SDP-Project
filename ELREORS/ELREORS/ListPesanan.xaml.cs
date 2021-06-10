0ad8e1db944bc01e2e36b908ad34058633ed7182
Davin <christopherdavin99@gmail.com>
2021-06-10 13:54:19 +0700

    Timer List, update

--- ELREORS/ELREORS/ListPesanan.xaml.cs
+++ ELREORS/ELREORS/ListPesanan.xaml.cs
@@ -24,11 +24,17 @@ namespace ELREORS
         DataTable dtPesanan, tempData;
         OracleDataAdapter daPesanan;
         OracleConnection conn;
+        int savedId = -1;
+        
         public ListPesanan()
         {
             InitializeComponent();
             this.WindowState = WindowState.Maximized;
             //this.WindowStyle = WindowStyle.None;
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Start();
             conn = App.conn;
 
             dtPesanan = new DataTable();
@@ -37,21 +43,31 @@ namespace ELREORS
             dtPesanan.Columns.Add("Jumlah");
             dtPesanan.Columns.Add("Keterangan");
             dtPesanan.Columns.Add("Status");
-            dtPesanan.Columns.Add("Action");
             loadData();
 
         }
 
         private void dgPesanan_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            
+        }
+
+        public void hapusData()
+        {
+            //tempData.Rows[dgPesanan.SelectedIndex].Delete();
+            //dtPesanan.Rows[dgPesanan.SelectedIndex].Delete();
+           
+        }
+
+        private void btnSelesai_Click(object sender, RoutedEventArgs e)
+        {
             try
             {
                 int index = Int32.Parse(tempData.Rows[dgPesanan.SelectedIndex][0].ToString());
-                
+
                 string qry = $"update djual set status=2 where id={index}";
-                
-                
-         
+
+                savedId = -1;
                 conn.Open();
                 OracleCommand cmd = new OracleCommand(qry, conn);
                 cmd.ExecuteNonQuery();
@@ -64,13 +80,29 @@ namespace ELREORS
                 MessageBox.Show(ex.Message.ToString());
             }
         }
-
-        public void hapusData()
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            //tempData.Rows[dgPesanan.SelectedIndex].Delete();
-            //dtPesanan.Rows[dgPesanan.SelectedIndex].Delete();
-           
+            loadData();
+            int tempInd = 0;
+            if (savedId != -1)
+            {
+                foreach (DataRow dr in tempData.Rows)
+                {
+                    if (dr[0].ToString() == savedId.ToString())
+                    {
+                        dgPesanan.SelectedIndex = tempInd;
+                    }
+                    tempInd++;
+                }
+            }
+            
         }
+
+        private void dgPesanan_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            savedId = Int32.Parse(tempData.Rows[dgPesanan.SelectedIndex][0].ToString());
+        }
+
         public void loadData()
         {
             try
@@ -83,10 +115,10 @@ namespace ELREORS
                 dtPesanan.Columns.Add("Nomor Meja");
                 dtPesanan.Columns.Add("Jumlah");
                 dtPesanan.Columns.Add("Keterangan");
-                dtPesanan.Columns.Add("Action");
+                dtPesanan.Columns.Add("Status");
 
                 tempData = new DataTable();
-                daPesanan = new OracleDataAdapter("select d.id, m.nama, h.nomor_meja, d.jumlah, d.keterangan, d.status from menu m, djual d, hjual h where d.id_header=h.id and d.id_menu=m.id and h.status=1 and d.status=1 order by d.status, h.nomor_meja", conn);
+                daPesanan = new OracleDataAdapter("select d.id, m.nama, h.nomor_meja, d.jumlah, d.keterangan, d.status from menu m, djual d, hjual h where d.id_header=h.id and d.id_menu=m.id and h.status=2 and d.status=1 order by d.status, h.nomor_meja", conn);
                 daPesanan.Fill(tempData);
                 foreach (DataRow d in tempData.Rows) 
                 {
@@ -100,7 +132,7 @@ namespace ELREORS
                     {
                         ket = d["keterangan"].ToString();
                     }
-                    dtPesanan.Rows.Add(d["nama"],"Meja "+d["nomor_meja"],d["jumlah"],ket,"-");
+                    dtPesanan.Rows.Add(d["nama"],"Meja "+d["nomor_meja"],d["jumlah"],ket,"In Process");
                 }
                 dgPesanan.ItemsSource = dtPesanan.DefaultView;
                 conn.Close();
